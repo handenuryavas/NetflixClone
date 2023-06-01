06977fc4d87d35bb812d38150b4c655ad52f058b
handenuryavas <hande.nur038@gmail.com>
2023-06-01 15:02:36 +0300

    first commit

---
+++ NetflixClone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by Hande Nur Yavaş on 1.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
